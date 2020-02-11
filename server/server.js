9010cf0b6832f79e2f56b3a4cf327dbb9093ca5f
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-02-11 01:10:31 -0500

    All components are rendering through proxy

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('./public'));
 
+//availability proxy
 var availabilityOptions = {
   target: 'http://localhost:3003',
   changeOrigin: true,
@@ -19,8 +20,24 @@ var availabilityOptions = {
     'http://localhost:3000' : 'http://localhost:3003'
   }
 }
-var testProxy = proxy(availabilityOptions);
-app.use('/getAllMatchingBreed', testProxy);
+var availabilityProxy = proxy(availabilityOptions);
+app.use('/getAllMatchingBreed', availabilityProxy);
+
+
+//info proxy
+var infoOptions = {
+  target: 'http://localhost:3002',
+  changeOrigin: true,
+  ws: true,
+  pathRewrite: {
+    '/api/oneBreed/': 'http://localhost:3002/api/oneBreed/', // rewrite path
+  },
+  router: {
+    'http://localhost:3000' : 'http://localhost:3002'
+  }
+}
+var infoProxy = proxy(infoOptions);
+app.use('/api/oneBreed/', infoProxy);
 
 //httpexpressmiddleware
 
