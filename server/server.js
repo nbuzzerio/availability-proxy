b142455e56c0082dfa5c3b7c22202145d2464dc1
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-03-06 20:15:08 -0500

    added redirect for dogId in URL

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(express.static('./public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/index.html'));
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`The proxy-server is Running on port ${port}!`));
